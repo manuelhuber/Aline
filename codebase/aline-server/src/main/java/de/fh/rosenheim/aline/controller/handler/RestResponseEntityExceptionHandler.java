f3b2e19bfee49f6b97602317a3ea968df1c71e32
Manuel Huber <nelo112@web.de>
2016-11-21 19:15:40 +0100

    Fixed authentication error handler

--- codebase/aline-server/src/main/java/de/fh/rosenheim/aline/controller/handler/RestResponseEntityExceptionHandler.java
+++ codebase/aline-server/src/main/java/de/fh/rosenheim/aline/controller/handler/RestResponseEntityExceptionHandler.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -27,9 +28,8 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
                 .body(new ErrorResponse("Something very unexpected happened"));
     }
 
-    @ExceptionHandler(AuthenticationException.class)
-    protected ResponseEntity<?> authorizationError(Exception exception) {
-        log.error("Unhandled exception: ", exception);
+    @ExceptionHandler(value = {AuthenticationException.class, AccessDeniedException.class})
+    protected ResponseEntity<?> authorizationError(RuntimeException exception) {
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
                 .body(new ErrorResponse("You do not have access."));
