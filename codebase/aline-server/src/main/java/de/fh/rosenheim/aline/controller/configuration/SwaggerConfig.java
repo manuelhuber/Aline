740fc5a88056832855626535ccfbcee63fec0c4d
Manuel Huber <nelo112@web.de>
2016-11-23 10:25:00 +0100

    Updated swagger to use api_key field for token

--- codebase/aline-server/src/main/java/de/fh/rosenheim/aline/controller/configuration/SwaggerConfig.java
+++ codebase/aline-server/src/main/java/de/fh/rosenheim/aline/controller/configuration/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package de.fh.rosenheim.aline.controller.configuration;
 
 import com.fasterxml.classmate.TypeResolver;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,8 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.WildcardType;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
+import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
@@ -21,6 +24,8 @@ import static springfox.documentation.schema.AlternateTypeRules.newRule;
 public class SwaggerConfig {
 
     private final TypeResolver typeResolver;
+    @Value("${token.header}")
+    private String tokenHeader;
 
     public SwaggerConfig(TypeResolver typeResolver) {
         this.typeResolver = typeResolver;
@@ -35,10 +40,27 @@ public class SwaggerConfig {
                 .build()
                 .genericModelSubstitutes(ResponseEntity.class)
                 .alternateTypeRules(
+                        // ResponseEntity<Foo> should be display as Foo
                         newRule(typeResolver.resolve(DeferredResult.class,
                                 typeResolver.resolve(ResponseEntity.class, WildcardType.class)),
                                 typeResolver.resolve(WildcardType.class)),
+                        // Iterable<Foo> should be displayed as List<Foo> since Spring Boot automatically serializes
+                        // Iterables as List/Array
                         newRule(typeResolver.resolve(Iterable.class, WildcardType.class),
                                 typeResolver.resolve(List.class, WildcardType.class)));
+    }
+
+    @Bean
+    SecurityConfiguration security() {
+        // This is copied from the springfox docu. We only need to change the header key
+        return new SecurityConfiguration(
+                "test-app-client-id",
+                "test-app-client-secret",
+                "test-app-realm",
+                "test-app",
+                "",
+                ApiKeyVehicle.HEADER,
+                tokenHeader,
+                "," /*scope separator*/);
     }
 }
