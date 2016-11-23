f205378b94278156d0255cce37294567a13a0bec
Manuel Huber <nelo112@web.de>
2016-11-23 10:47:33 +0100

    Added exception handling & swagger comments

--- codebase/aline-server/src/main/java/de/fh/rosenheim/aline/controller/rest/UserController.java
+++ codebase/aline-server/src/main/java/de/fh/rosenheim/aline/controller/rest/UserController.java
@@ -1,14 +1,17 @@
 package de.fh.rosenheim.aline.controller.rest;
 
+import de.fh.rosenheim.aline.controller.util.SwaggerTexts;
 import de.fh.rosenheim.aline.model.domain.User;
 import de.fh.rosenheim.aline.model.exceptions.NoObjectForIdException;
+import de.fh.rosenheim.aline.model.json.response.ErrorResponse;
 import de.fh.rosenheim.aline.service.UserService;
 import de.fh.rosenheim.aline.util.ControllerUtil;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PostAuthorize;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -26,9 +29,21 @@ public class UserController {
 
     @RequestMapping(method = RequestMethod.GET)
     @PostAuthorize("@securityService.canAccessUserData(principal, returnObject)")
-    public User user(@RequestParam(required = false, name = "name") String queryName, HttpServletRequest request)
-            throws NoObjectForIdException {
+    @ApiOperation(value = "get user info", notes = "Returns the data of the current user (detected via token)")
+    public User user(
+            @ApiParam(value = SwaggerTexts.OTHER_USER) @RequestParam(required = false, name = "name") String queryName,
+            HttpServletRequest request) throws NoObjectForIdException {
         String name = queryName != null ? queryName : controllerUtil.getUsername(request);
         return userService.getUserByName(name);
+    }
+
+    /**
+     * Custom response if no User for the given name exists
+     */
+    @ExceptionHandler(NoObjectForIdException.class)
+    public ResponseEntity<ErrorResponse> noObjectException(NoObjectForIdException ex) {
+        return new ResponseEntity<>(
+                new ErrorResponse("No user with name=" + ex.getId()),
+                HttpStatus.NOT_FOUND);
     }
 }
---
+++ codebase/aline-server/src/main/java/de/fh/rosenheim/aline/controller/util/SwaggerTexts.java
@@ -0,0 +1,9 @@
+package de.fh.rosenheim.aline.controller.util;
+
+/**
+ * Created by Manuel on 23.11.2016.
+ */
+public class SwaggerTexts {
+
+    public final static String OTHER_USER = "Accessing other users data requires specific authorities";
+}
