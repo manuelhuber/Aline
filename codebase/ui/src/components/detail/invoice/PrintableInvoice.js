547680cae78ccef382774096bda82292245b24c7
binarykittenDE <franziska.haaf@gmail.com>
2017-01-22 15:23:10 +0100

    Added seminar infos for the invoice.

--- codebase/ui/src/components/detail/invoice/PrintableInvoice.js
+++ codebase/ui/src/components/detail/invoice/PrintableInvoice.js
@@ -8,6 +8,8 @@ export class PrintableInvoice extends React.Component {
     constructor(props) {
         super(props);
         this.renderCosts = this.renderCosts.bind(this);
+        this.renderDates = this.renderDates.bind(this);
+        this.renderSeminarInfos = this.renderSeminarInfos.bind(this);
         this.state = {
             seminar: {},
             participants: [],
@@ -34,6 +36,47 @@ export class PrintableInvoice extends React.Component {
             });
     }
 
+    renderDates(date) {
+        let formattedDate = new Date(date).toLocaleDateString();
+        return (
+            <span key={formattedDate} className="date">
+                {formattedDate}
+            </span>
+        )
+    }
+
+    renderSeminarInfos() {
+        return (
+            <div className="seminar-infos">
+                <h1>Rechnung</h1>
+                <div>
+                    <output title="Name des Seminars">
+                        <label>Seminar</label>
+                        <span>{this.state.seminar.name}</span>
+                    </output>
+                </div>
+                <div>
+                    <output title="Termine des Seminars">
+                        <label>Termine</label>
+                        {this.state.seminar.dates && this.state.seminar.dates.map(this.renderDates)}
+                    </output>
+                </div>
+                <div >
+                    <output title="Die Location wo das Seminar stattfand">
+                        <label>Location</label>
+                        <span>Fehlt</span>
+                    </output>
+                </div>
+                <div >
+                    <output title="Der Trainer der das Seminar führte">
+                        <label>Trainer</label>
+                        <span>{this.state.seminar.trainer}</span>
+                    </output>
+                </div>
+            </div>
+        )
+    }
+
     renderCosts() {
         return (
             <div className="costs">
@@ -52,7 +95,9 @@ export class PrintableInvoice extends React.Component {
                 <div >
                     <output title="Kosten pro Teilnehmer am Seminar">
                         <label>Kosten/TN</label>
-                        <span className="money-amount">{Util.formatMoneyFromCent(this.state.seminar.costsPerParticipant)} €</span>
+                        <span
+                            className="money-amount">{Util.formatMoneyFromCent(this.state.seminar.costsPerParticipant)}
+                            €</span>
                     </output>
                 </div>
             </div>
@@ -62,11 +107,10 @@ export class PrintableInvoice extends React.Component {
     render() {
         return (
             <div className="invoice">
-                Your Invoice for the seminar with id {this.props.params.seminarId} here
+                {this.renderSeminarInfos()}
                 <h3>Teilnehmerliste</h3>
                 <Participants participants={this.state.participants}
                               costsPerParticipant={this.state.seminar.costsPerParticipant}/>
-                <h3>Kosten</h3>
                 {this.renderCosts()}
                 <h3>Kosten pro Bereich</h3>
                 <Divisions divisionSums={this.state.divisionSums}/>
